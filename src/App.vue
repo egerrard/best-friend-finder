d857ba48336ab7ff8c845c22add11c7c1ca89f7f
Eric Gerrard <ericgerrard@me.com>
2018-03-24 14:51:37 -0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <img src="./assets/best-friends.jpg">
+    <BestFriendFinder msg="Best Friend Finder"/>
+  </div>
+</template>
+
+<script>
+import BestFriendFinder from './components/BestFriendFinder.vue'
+
+export default {
+  name: 'app',
+  components: {
+    BestFriendFinder
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+
+}
+body {
+  background-color:#fffce3;
+}
+
+</style>
---
+++ src/components/BestFriendFinder.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>
+      Sick and tired of not having enough best friends?  Find yourself a new best friend now!
+    </p>
+    <h3>Installed CLI Plugins</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BestFriendFinder',
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
